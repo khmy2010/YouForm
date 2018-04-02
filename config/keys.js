b85fc6e53ce18258495c82fc249a07dc00969474
khmy2010 <kehan.see@gmail.com>
2018-04-02 08:06:40 +0800

    travis keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,9 @@
 if (process.env.NODE_ENV === 'production') {
     //production environment
     module.exports = require('./prod');
+} else if (process.env.NODE_ENV === 'ci') {
+    //CI environment
+    module.exports = require('./ci');
 } else {
     //development environment
     module.exports = require('./dev');
