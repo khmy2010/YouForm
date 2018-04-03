9b39af8f30da34debc65f70991c6eb8da254e4e7
khmy2010 <kehan.see@gmail.com>
2018-04-03 10:04:18 +0800

    polished tests

--- tests/factories/user.js
+++ tests/factories/user.js
@@ -5,7 +5,7 @@ const User = mongoose.model('user');
 
 //generate a new user with random googleId and fixed name
 module.exports = async () => {
-    await User.deleteMany({ googleId: { $in: [/TEST/] } }); //clean up users in test database before proceed
+    // await User.deleteMany({ googleId: { $in: [/TEST/] } }); //clean up users in test database before proceed
     return new User({
         googleId: 'TEST' + uuidv4(),
         name: 'Mr Jest'
