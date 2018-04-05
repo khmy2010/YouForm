f20e2d6677d4710ac8ee563b82dcb04afdbd408b
khmy2010 <kehan.see@gmail.com>
2018-04-05 22:58:16 +0800

    removed tests for home page

--- tests/interface.test.js
+++ tests/interface.test.js
@@ -29,16 +29,6 @@ describe('user has logged in', async () => {
         await page.login();
     });
 
-    it('should able to see the home page', async () => {
-        const text = await page.getSelected('#Landing__Intro');
-        expect(text.length).toBeGreaterThan(0);
-    });
-
-    it('should able to see the correct document title', async () => {
-        const text = await page.title();
-        expect(text).toEqual('YouForm');
-    });
-
     it('should able to see the logout button', async () => {
         const text = await page.getSelected('a[href="/api/logout"]');
         expect(text).toEqual('Logout');
