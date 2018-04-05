c33d6e644f23ab321bc4b3c68a14bd090b7c4dce
khmy2010 <kehan.see@gmail.com>
2018-04-05 22:42:36 +0800

    first commit on redesigned landing page

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -3,17 +3,27 @@ import React from 'react';
 import Header from '../Header/Header';
 
 import './Landing.css';
+import Connecting from '../../assets/images/landing.svg';
 
 const landing = ({ auth }) => {
     return (
         <React.Fragment>
-            <Header auth={auth} />
-            <div className="Landing">
-                <section id="Landing__Title">
-                    <h1 id="Landing__Intro">
-                        Changing how you ask for information
-                    </h1>
-                </section>
+            <div className="Landing__Wrapper">
+                <Header auth={auth} />
+                <div className="Landing">
+                    <div className="Landing__Text">
+                        <h1>Because we are human.</h1>
+                        <p className="Landing__Intro">
+                            YouForm changes the way you ask by introducing forms
+                            that converse with human that makes the process more
+                            human.
+                        </p>
+                    </div>
+                    <img
+                        src={Connecting}
+                        alt="YouForm helps student to collect data better."
+                    />
+                </div>
             </div>
         </React.Fragment>
     );
