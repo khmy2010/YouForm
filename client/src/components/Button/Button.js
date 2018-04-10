a77fc01081854c6989ac003fea263f16a2b6b055
khmy2010 <kehan.see@gmail.com>
2018-04-10 12:52:39 +0800

    preparation for stencils and context

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 import './Button.css';
 
-const button = props => (
-    <button
-        className="EleButton"
-        onClick={props.clicked}
-        disabled={props.disabled}
-    >
-        {props.children}
-    </button>
-);
+const button = props => {
+    let classNames = ['EleButton'];
+
+    if (props.className) classNames.push(props.className);
+
+    classNames = classNames.join(' ');
+
+    return (
+        <button
+            className={classNames}
+            onClick={props.clicked}
+            disabled={props.disabled}
+        >
+            {props.children}
+        </button>
+    );
+};
 
 export default button;
