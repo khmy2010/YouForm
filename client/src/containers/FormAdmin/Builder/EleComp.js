8c2694b6752e22a8336fbe428d1228ebf06c7d5d
khmy2010 <kehan.see@gmail.com>
2018-05-23 09:14:58 +0800

    removed comments

--- client/src/containers/FormAdmin/Builder/EleComp.js
+++ client/src/containers/FormAdmin/Builder/EleComp.js
@@ -16,21 +16,6 @@ const eleComp = props => {
                     init={props.value}
                 />
             );
-        // return (
-        //     <div className="EleCheck">
-        //         <input
-        //             id={props.id}
-        //             type={props.type}
-        //             name={props.name}
-        //             value={props.value}
-        //             onChange={event => props.onCheckboxChange(event)}
-        //         />
-        //         <label htmlFor={props.id}>
-        //             <span />
-        //             {props.displayName}
-        //         </label>
-        //     </div>
-        // );
         case 'input':
             return (
                 <div className="EleField">
